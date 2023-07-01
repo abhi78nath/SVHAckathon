b5f41fc95727fc2933015917576b6ca970469711
abhishek nath <abhishek78nath@gmail.com>
2023-07-01 19:29:22 +0530

    scrapperfile

--- server/index.js
+++ server/index.js
@@ -1,12 +1,20 @@
-const express = require('express')
+const express = require('express');
+const { scrapeDribbbleProfile } = require('./scrappers/dribbleScrapper');
 
 const app = express();
-
 const port = 5000;
 
-app.get('/' , (req, res)=> {
-    res.send("Welcome")
-})
-app.listen(port, (req,res) => {
-    console.log(`Server is running at http://127.0.0.1:${port}`);
-})
+const username = 'stepanovdesign';
+
+app.get('/', async (req, res) => {
+  try {
+    const data = await scrapeDribbbleProfile(username);
+    res.json(data);
+  } catch (error) {
+    res.status(500).json({ error: 'An error occurred' });
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Server is running at http://127.0.0.1:${port}`);
+});
---
+++ server/scrappers/dribbleScrapper.js
@@ -0,0 +1,44 @@
+// scraper.js
+
+const puppeteer = require('puppeteer');
+
+async function scrapeDribbbleProfile(username) {
+  try {
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+
+    const url = `https://dribbble.com/${username}`;
+    await page.goto(url);
+
+    await page.waitForSelector('.work-details');
+    await page.waitForSelector('.shot-thumbnail-title');
+    await page.waitForSelector('.shot-thumbnail-image');
+    await page.waitForSelector('.skill-name');
+
+    const workDetails = await page.$eval('.work-details', (element) => element.textContent.trim());
+    const projects = await page.$$eval('.shot-thumbnail-title', (elements) =>
+      elements.map((element) => element.textContent.trim())
+    );
+    const likedShots = await page.$$eval('.shot-thumbnail-image', (elements) =>
+      elements.map((element) => element.src)
+    );
+    const skills = await page.$$eval('.skill-name', (elements) =>
+      elements.map((element) => element.textContent.trim())
+    );
+
+    await browser.close();
+
+    const scrapedData = {
+      workDetails,
+      projects,
+      likedShots,
+      skills,
+    };
+
+    return scrapedData;
+  } catch (error) {
+    throw error;
+  }
+}
+
+module.exports = { scrapeDribbbleProfile };
