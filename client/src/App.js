dc28caebfccc581c826a12093fa1cc0e4f36d68e
bikdistinct <bikdev07@gmail.com>
2023-07-02 23:14:04 +0530

    one way auth done

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,29 @@
 import "./App.css";
+import React from "react";
+import { BrowserRouter as Router, Routes, Route} from "react-router-dom";
+
 import ProfileDetails from "./components/profileForm/profileData";
-import Auth from "./components/Auth/Login";
+// import Auth from "./components/Auth/LoginP";
+import Signup from "./components/Auth/signup";
+import Login from "./components/Auth/Login";
 import Profile from "./components/profileUi/profileUi";
 import Home from "./components/Home/Home";
 import Results from "./components/Results/ResultsUi";
+import Navbar from "./components/Navbar/Navbar";
 function App() {
   return (
     <div className="App">
-      {/* <ProfileDetails /> */}
-      {/* <Auth/> */}
-      {/* <Profile/> */}
-      <Home/>
-      {/* <Results /> */}
+      <Router>
+      <Navbar/>
+      <Routes>
+        <Route path="/" element={<Home/>} />
+        <Route path="/socialLinks" element={<ProfileDetails />} />
+        <Route path="/login" element={<Login/>} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/results" element={<Results/>}/>
+      </Routes>
+      </Router>
     </div>
   );
 }
--- client/src/components/Auth/Login.jsx
+++ client/src/components/Auth/Login.jsx
@@ -1,63 +1,74 @@
 import React from 'react'
 import "./login.css";
-function Login() {
+
+import { useState, useEffect } from "react";
+import axios from "axios";
+
+
+function Signup() {
+ const [email,setEmail]=useState();
+ const [password,setPassword]=useState();
+
+ const submithandler=async()=>{
+    console.log(name,email,password,role);
+
+    try{
+        const config={
+            headers:{
+                "Content-Type": "application/json",
+            },
+        };
+
+        const {data}=await axios.post("http://localhost:5000/candidate/createuser",{
+            name,
+            email,
+            password,
+            role,
+        },config);
+
+        console.log(data);
+        localStorage.setItem("userInfo", JSON.stringify(data));
+    } catch(error) {
+    throw(error);
+ }
+ };
+//  handle submiters end
+
+const handleRoleSelection = (selectedRole) => {
+    setRole(selectedRole);
+  };
 
 
   return (
     <div className='auth'>  
-         <form>
-            <div className='signup'>
-                <h2>Sign Up</h2>
-                    <div className="Name_label">
-                        <label for="name">Name</label>
-                        <input type="text" id="name" name="name" />
-                    </div>
+<form id="loginForm">
+        <div className="login">
+          <h2>Sign In</h2>
 
-                    <div className="email_label">
-                        <label for="email">E-Mail</label>
-                        <input type="email" id="email" name="email" />
-                    </div>
+          <div className="email_label">
+            <label htmlFor="email2">E-Mail</label>
+            <input type="email" id="email2" name="email2" onChange={(e) => setEmail(e.target.value)} required/>{" "}
+          </div>
 
-                    <div className="password_label">
-                        <label for="password">Password</label>
-                        <input type="password" id="password" name="password" />
-                    </div>
+          <div className="password_label">
+            <label htmlFor="password2">Password</label>
+            <input type="password" id="password2" name="password2" onChange={(e) => setPassword(e.target.value)} required />{" "}
+          </div>
 
-                    <div className="role_label">
-                        <div className='recruiter'>Recruiter</div>
-                        <div className='candidate'>Candidate</div>
-                    </div>                                                            
-            </div>
-            </form>
-
-            <form>
-                <div className='login'>
-                    <h2>Sign In</h2>
-
-                    <div className="email_label">
-                        <label for="email">E-Mail</label>
-                        <input type="email" id="email" name="email" />{" "}
-                    </div>
-
-                    <div className="password_label">
-                        <label for="password">Password</label>
-                        <input type="password" id="password" name="password" />{" "}
-                    </div>
-
-                    <div className="role_label">
-                        <div className='recruiter'>Recruiter</div>
-                        <div className='candidate'>Candidate</div>
-                    </div>
-                </div>
-            </form>
-
-        <div className="btnsubmit">
-            <button type="submit" className="profileDataBtn">
+          <div className="role_label">
+            <div className="recruiter" onClick={() => handleRoleSelection('recruiter')}>Recruiter</div>
+            <div className="candidate" onClick={() => handleRoleSelection('candidate')}>Candidate</div>
+          </div>
+        </div>
+      </form>
+            <div className="btnsubmit">
+            <button type="submit" className="profileDataBtn" onClick={submithandler} form="loginForm" >
                      Submit &rarr;
             </button>
       </div>
-    </div>
+            </div>
+            
   )
 }
 
-export default Login
+export default Signup;
